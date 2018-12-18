9a73d42ca5d028e4c4f98ded1a8cfcfb3a353d87
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-18 17:23:43 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hi": "hello",
+    "2": "to", "two", "too",
+    "4": "for", "four",
+    "b": "be",
+    "u": "you",
+    "@": 
+  }
