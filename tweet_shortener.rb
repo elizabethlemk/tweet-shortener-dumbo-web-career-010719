cb678e707152f20c6b8866c1a0efbe0542fbf774
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-18 17:25:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def dictionary
   dictionary = {
-    "hi": "hello",
-    "2": "to", "two", "too",
-    "4": "for", "four",
-    "b": "be",
-    "u": "you",
-    "@": "at",
-    "&": "and"
+    :"hi" => "hello",
+    :"2" => "to", "two", "too",
+    :"4" => "for", "four",
+    :"b" => "be",
+    :"u" => "you",
+    :"@" => "at",
+    :"&" => "and"
   }
 end
